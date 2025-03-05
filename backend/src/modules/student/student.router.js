acbb57cb1761376eb4f828fe7f5376899c788c52
AbdelrahmanMohasseb <abdelrahmanmohasseb2712000@gmail.com>
2025-03-05 20:26:38 +0200

    update signup

--- backend/src/modules/student/student.router.js
+++ backend/src/modules/student/student.router.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {
-    createStudent,getStudentById,getAllStudents,updateStudent,deleteStudent} 
+const {createStudent,getStudentById,getAllStudents,updateStudent,deleteStudent,signup} 
     = require("../../../src/modules/student/controller/student.controller");
+const { validateStudentSignup } = require("./student.validator"); 
 
 router.post("/", createStudent); 
 router.get("/", getAllStudents); 
 router.get("/:id", getStudentById); 
 router.put("/:id", updateStudent);
 router.delete("/:id", deleteStudent);
+router.post("/signup", validateStudentSignup, signup);
 
 
 
