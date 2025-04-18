8348853c20630e9f6fb00fd4919cfc53dcd5031e
AbdelrahmanMohasseb <abdelrahmanmohasseb2712000@gmail.com>
2025-04-18 15:13:08 +0200

    Login 4

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.post("/api/payments/webhook",
 
 
 // Routes
-app.use("/api/Login", loginRoutes);
+app.use("/api/Login", require("./src/modules/Login/Login.router"));
 app.use('/api/students',studentRoutes)
 app.use("/api/advisors", advisorRoutes);
 app.use("/api/organizers", organizerRoutes);
